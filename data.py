4d9326f9dc32d2cc66c14d337e8730823c9f8cd3
hussamh10 <hussamh10@gmail.com>
2017-10-14 02:43:40 +0500

    Decreased data

--- data.py
+++ data.py
@@ -23,27 +23,27 @@ def getFusionData():
     images, _, _, _ = getVideoData()
 
     all_images = np.append(images, images)
-    all_images = all_images.reshape((3200, 224, 224, 3))
+    all_images = all_images.reshape((1600, 224, 224, 3))
     
     
     pair_labels = []
-    for _ in range(1600):
+    for _ in range(800):
         pair_labels.append(0)
 
     print(all_images.shape)
 
     data = labels = []
 
-
     audio1, _, _, _ = getAudioData()
     audio2, _, _, _ = getAudioData('.\\data\\audio\\airplanes\\', '.\\data\\audio\\airplanes\\' )
 
     all_audio = np.append(audio1, audio2)
-    all_audio = all_audio.reshape((3200, 199, 257, 3))
+
+    all_audio = all_audio.reshape((1600, 199, 257, 3))
 
     pairs = [all_images, all_audio]
 
-    for _ in range(1600):
+    for _ in range(800):
         pair_labels.append(1)
 
     pair_labels = utils.to_categorical(pair_labels, 2)
@@ -58,8 +58,8 @@ def getAudioData(dir1 = '.\\data\\audio\\airplanes\\' , dir2 = '.\\data\\audio\\
     data, labels = readImages(dir1, 0, 800, data, labels, '.jpg')
     data, labels = readImages(dir2, 1, 800, data, labels, '.jpg')
 
-    train_data = data[:1600]
-    train_label = labels[:1600]
+    train_data = data[:800]
+    train_label = labels[:800]
 
     test_data = data[1000:1100]
     test_label = labels[1000:1100]
@@ -88,8 +88,8 @@ def getVideoData():
     data, labels = readImages('.\\data\\video\\airplanes\\' , 0, 800, data, labels, '.jpg')
     data, labels = readImages('.\\data\\video\\motorbikes\\', 1, 800, data, labels, '.jpg')
 
-    train_data = data[:1600]
-    train_label = labels[:1600]
+    train_data = data[:800]
+    train_label = labels[:800]
 
     test_data = data[1000:1100]
     test_label = labels[1000:1100]
